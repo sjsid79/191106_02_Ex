24f1e7f9cdb56867271e393b6404b723d6c2a31c
sjsid79 <wjdtlr0723>
2019-11-06 22:17:12 +0900

    Initial commit

---
+++ app/src/main/java/com/tjoeun/a20191106_02_ex/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.tjoeun.a20191106_02_ex
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/tjoeun/a20191106_02_ex/datas/ItemData.kt
@@ -0,0 +1,9 @@
+package com.tjoeun.a20191106_02_ex.datas
+
+class ItemData(inputTitle:String, inputCompany:String) {
+
+    var title = inputTitle
+    var company = inputCompany
+
+
+}
